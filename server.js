7af548a284868754fc11fedd555a156b727062d5
algol007 <adhyformerz@gmail.com>
2020-05-04 00:15:42 +0800

    add: socket

--- server.js
+++ server.js
@@ -5,5 +5,24 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
+});
+
+// io.on('connection', () => {
+//   console.log('is connection');
+// });
+// io.on('event', (data) => {
+//   console.log('is event');
+//   console.log(data);
+// });
+// io.on('disconnect', () => {
+//   console.log('is disconnect');
+// });
 
 server.listen(port);
