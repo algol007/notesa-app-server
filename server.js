dc2036ee885a7df7fc9b1c4b1a72a777317f8391
algol007 <adhyformerz@gmail.com>
2020-05-04 10:49:16 +0800

    fix: sockets

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data);
-  });
+// io.on('connection', (socket) => {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', (data) => {
+//     console.log(data);
+//   });
+// });
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id)
+    socket.on('SEND_MESSAGE', function(data) {
+        io.emit('MESSAGE', data)
+    });
 });
 
 // io.on('connection', () => {
